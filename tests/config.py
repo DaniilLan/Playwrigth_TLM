afca2bbd0464ecdfe4cf0c9600b6c1d8223265a2
Daniil <landan2001@mail.ru>
2024-07-30 10:54:48 +0400

    Update config.py

--- tests/config.py
+++ tests/config.py
@@ -10,25 +10,25 @@ url_support_test = 'http://192.168.7.221:8081/support'
 
 list_url_test = [url_support_test, url_help_test, url_auth_test, url_users_test]
 
-password_all = '12345678'
-invalid_pass = '12345687'
+password_all = ''
+invalid_pass = ''
 
-cred = {"adm@adm.com": 'Админ Телемедцентра',
-        "spadm@adm.adm": 'Админ Скорой',
-        "testadm@adm.adm": 'Админ Црб',
-        "testdoc@doc.doc": 'Доктор Телемедцентра',
-        "doc@doc.com": 'Доктор Скорой',
-        "testdoc@doc.com": 'Доктор Црб',
-        "d.s.ivanov1@samsmu.ru": 'Доктор Фап'}  # Без медработник ФАП и Пациент Фап.
+cred = {"m": 'Админ Телемедцентра',
+        "m": 'Админ Скорой',
+        "dm": 'Админ Црб',
+        "tc": 'Доктор Телемедцентра',
+        "dom": 'Доктор Скорой',
+        "tm": 'Доктор Црб',
+        ".ru": 'Доктор Фап'}  # Без медработник ФАП и Пациент Фап.
 
-mails_doc = ["testdoc@doc.doc", "doc@doc.com", "testdoc@doc.com", "d.s.ivanov1@samsmu.ru"]
-mail_lan_doc = "TestLanDoc@doc.doc"
-mail_lan_adm = "TestLanAdm@mail.ru"
-mail_adm = "adm@adm.com"
-mail_doc = "testdoc@doc.doc"
-mails_adm = ["adm@adm.com", "spadm@adm.adm", "testadm@adm.adm"]
-invalid_mail = "123123@mail.ru"
-valid_mail = 'landan2001@mail.ru'
+mails_doc = ["u"]
+mail_lan_doc = "Tdoc"
+mail_lan_adm = "Te.ru"
+mail_adm = "am"
+mail_doc = "tdoc"
+mails_adm = ["adm"]
+invalid_mail = "u"
+valid_mail = 'lu'
 
 
 def random_phone():
