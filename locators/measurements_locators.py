c9e999085bec0697edfebba96e16ab1812515ada
dlancov <landan2001@mail.ru>
2025-05-06 13:36:06 +0400

    without .env and .ini

--- locators/measurements_locators.py
+++
@@ -1,4 +0,0 @@
-class LocatorsAllMeasurements:
-    REPORTS = '//*[@id="rootTelemedHub"]/div[2]/main/div/div/div[1]/div[2]/div/div/div/button'
-    TEXT_ALL_MEASUREMENTS = '//div/div[text()="Все измерения"]'
-
